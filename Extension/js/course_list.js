db4baef4db95a99292e828b832d5fe49c9640966
babramczyk <bjabramczyk@gmail.com>
2017-03-01 20:47:14 -0600

    Completed course_list.averageGPASetup() method, for first page load only (does not yet work on live queries)  (Task 1c)
    For now, we assume that model.getAverageGPA() takes in just one course as a parameter.

--- Extension/js/course_list.js
+++ Extension/js/course_list.js
@@ -2,16 +2,35 @@
 listSetup();
 filteringFieldsSetup();
 
-
 /***** setup functions *****/
 
 function listSetup() {
-  averageGpaSetup();
+  averageGPASetup();
   distributionSetup();
   sectionsListenerSetup();
 }
 
-function averageGpaSetup() {
+function averageGPASetup() {
+    // var aveGPAs = {};
+
+    // Iterates over each class -- sections button used as selector since it has an href with the corresponding course number inside
+    $('a.sectionExpand').each(function() {
+        // Table cell to eventually append the average GPA to
+        var addlInfoTD = $(this).parent().parent().prev().prev().children('td:nth-child(7)');
+
+        // Extracts course ID
+        var courseURL = $(this).attr('href');
+        var courseID = courseURL.match(/courseID=([0-9]*)/)[1];
+        courseID.replace("courseID=", "");
+
+        // Ask the model for the average GPA for this class, append it to the TD
+        chrome.runtime.sendMessage( {
+            action: "getAverageGPA",
+            course: courseID
+        }, function(response) {
+            addlInfoTD.append("<strong>Ave. GPA: </strong>" + response.aveGPA + "<br>");
+        })
+    })
 
 }
 
