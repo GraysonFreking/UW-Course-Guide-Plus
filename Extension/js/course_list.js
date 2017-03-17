31ab1e931adc986787b9ca09ff25a549f6299087
emcleod17 <emcleod@wisc.edu>
2017-03-16 20:19:06 -0500

    course_list.js quick commit
    ~I gotta commit this quick to pull some changes. Pay no mind.

--- Extension/js/course_list.js
+++ Extension/js/course_list.js
@@ -43,19 +43,13 @@ function distributionSetup() {
     $.initialize('a.sectionExpand', function() {
         //make sure it's not one of the links we inserted
         if ($(this).attr('href') !== "javascript:void(0)") {
+            
             //creating the new link to be appended
             var newLink = document.createElement("a");
             newLink.href = "javascript:void(0)";
             newLink.innerHTML = "grades";
-            newLink.className = "sectionExpand hide collapsibleCriteria enabled";
-            newLink.onclick = function () {
-                //write the expandSection function here
-                
-                //create skeleton table
-                var distTable = document.createElement("table");
-                //append skeleton table to section
-                $(this).parent().append(distTable);
-                
+            newLink.className = "sectionExpand hide collapsibleCriteria enabled distExpandLink";
+            /*newLink.onclick = function () {
                 //get courseID
                 var courseURL = $(this).attr('href');
                 var courseID = courseURL.match(/courseID=([0-9]*)/)[1];
@@ -63,7 +57,7 @@ function distributionSetup() {
                 
                 //call distributionExpanded which makes DB request
                 distributionExpanded(courseID);
-            }
+            }*/
             
             //quick create the arrow icon
             var arrowIcon = document.createElement("img");
@@ -73,8 +67,40 @@ function distributionSetup() {
             arrowIcon.height = "15";
             newLink.appendChild(arrowIcon);
             
+            //add listener to newLink
+            newLink.addEventListener('click', function () {
+                 //get courseID
+                var courseURL = $(this).attr('href');
+                var courseID = courseURL.match(/courseID=([0-9]*)/)[1];
+                courseID.replace("courseID=", "");
+                
+                //call distributionExpanded which makes DB request
+                distributionExpanded(courseID);
+                
+                //test
+                alert("you clicked it");
+                console.log("clicked the new link");
+            });
+            
             //append newLink to the section, below the sections link
             $(this).parent().append(newLink);
+            
+            
+            /*//create the hidden div
+            var tableDiv = $(document.createElement("div")).hide();
+            tableDiv.className = "distTableDivClass";
+            ////tableDiv.display = "none";
+            //create the table to append to div
+            var distTable = document.createElement("table");
+            distTable.className = "distTableClass";
+            tableDiv.appendChild(distTable);
+            //testing
+            var testNode = document.createElement("p");
+            testNode.innerHTML = "Wow this is a neat test, check this out.";
+            tableDiv.appendChild(testNode);
+            //append div into the page
+            $(this).parent().append(tableDiv);*/
+            
         }
     })
     
@@ -104,5 +130,14 @@ function sectionsExpanded(sectionsTable) {
 
 function distributionExpanded(course) {
 
+    chrome.runtime.sendMessage( {
+        action: "getDistribution",
+        course: course,
+        count: 5
+    }, function(response) {
+        //do response handling here & fill build/fill table
+        var jsonObj = $.parseJSON(response);
+    });
+    
 }
 
