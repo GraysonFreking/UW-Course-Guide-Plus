b8a653fdcc0fb261e143e66cdf0ab6e307b047ae
Zach Petersen <zjpetersen@wisc.edu>
2017-04-06 11:34:25 -0500

    Navigate to course guide correctly

--- Testing/GradeDistributionTests.py
+++ Testing/GradeDistributionTests.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 class GradeDistribution(unittest.TestCase):
@@ -16,31 +17,37 @@ class GradeDistribution(unittest.TestCase):
     def setUpClass(inst):
         #Install extension
         chop = webdriver.ChromeOptions()
-        chop.add_argument("load-extension=../Extension")        
+        #chop.add_argument("load-extension=../Extension")        
 
         directory = os.path.dirname(os.path.abspath(__file__))
         inst.driver = webdriver.Chrome(executable_path = directory + '/chromedriver', chrome_options = chop)
         inst.driver.implicitly_wait(10)
-
+        inst.driver.set_page_load_timeout(10)
         #Navigate to course guide
         driver = inst.driver
         driver.get('https://my.wisc.edu/web/expanded')
-        time.sleep(1)
         url = 'https://login.wisc.edu/idp/profile/SAML2/Redirect/SSO'
         #login if not already logged in
         if (url in driver.current_url):
             inst.login(driver)
         try:
-            cg_link = driver.find_element_by_xpath("//md-card[@aria-label='Course Guide widget']//a[@class='btn btn-default launch-app-button ng-scope md-uw-madison-theme']")
-            time.sleep(1)
+            time.sleep(5)
+            cg_link = driver.find_element_by_link_text('Go to my course guide')
+            #Navigate to course guide link and click it. TODO: fails sometimes
+            ActionChains(driver).move_to_element(cg_link).perform()
             cg_link.click()
         except Exception as e:
             print(str(e))
             print("Failed to load the CG")
             driver.quit()
 
-        time.sleep(1)
-
+        #Switch to the course guide        
+        driver.switch_to_window(driver.window_handles[-1])
+        #Check if this worked
+        source = driver.page_source
+        if "Go to my course guide" in source:
+            print("Old page still")
+            driver.quit()
     #Runs before every test
     def setUp(self):
         pass
@@ -49,17 +56,14 @@ class GradeDistribution(unittest.TestCase):
     #Tests
     def test_cg_loaded(self):
         driver = self.driver
-        #TODO: Not working. Might be a timing issue
-        try:
-            course = driver.find_element_by_class_name('courseResult')
-            cg = driver.find_element_by_class_name('CG_Home')
-            cg = driver.find_element_by_id('CG_browsePluto_29_u360303l1n15_118316_tw_')
-            print(cg.get_attribute("class"))
-            print(cg.get_attribute("id"))
-            self.assertEqual("CG_browse", cg.get_attribute("class"))
-        except Exception as e:
-            print(str(e))
-            print("Can't get elements on CG")
+        source = driver.page_source
+        #Make sure do not have old page
+        self.assertTrue("Go to my course guide" not in source)
+        #And have the new page
+        self.assertTrue("CG_Home" in source)
+        #Make sure can get elements
+        cg = driver.find_element_by_id('CG_browsePluto_29_u360303l1n15_118316_tw_')
+        self.assertEqual("CG_browse", cg.get_attribute("class"))
         
 
     #tear down after each test
