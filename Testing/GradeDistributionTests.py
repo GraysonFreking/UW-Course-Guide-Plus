5f84a5c92e696aac79c8ab9d979c814bc848610f
Zach Petersen <zjpetersen@wisc.edu>
2017-04-06 13:41:22 -0500

    Add tests for ave GPA and grades dropdown

--- Testing/GradeDistributionTests.py
+++ Testing/GradeDistributionTests.py
@@ -17,7 +17,7 @@ class GradeDistribution(unittest.TestCase):
     def setUpClass(inst):
         #Install extension
         chop = webdriver.ChromeOptions()
-        #chop.add_argument("load-extension=../Extension")        
+        chop.add_argument("load-extension=../Extension")        
 
         directory = os.path.dirname(os.path.abspath(__file__))
         inst.driver = webdriver.Chrome(executable_path = directory + '/chromedriver', chrome_options = chop)
@@ -34,7 +34,9 @@ class GradeDistribution(unittest.TestCase):
             time.sleep(5)
             cg_link = driver.find_element_by_link_text('Go to my course guide')
             #Navigate to course guide link and click it. TODO: fails sometimes
+            time.sleep(1)
             ActionChains(driver).move_to_element(cg_link).perform()
+            time.sleep(1)
             cg_link.click()
         except Exception as e:
             print(str(e))
@@ -48,6 +50,8 @@ class GradeDistribution(unittest.TestCase):
         if "Go to my course guide" in source:
             print("Old page still")
             driver.quit()
+        else:
+            print("New page")
     #Runs before every test
     def setUp(self):
         pass
@@ -65,6 +69,36 @@ class GradeDistribution(unittest.TestCase):
         cg = driver.find_element_by_id('CG_browsePluto_29_u360303l1n15_118316_tw_')
         self.assertEqual("CG_browse", cg.get_attribute("class"))
         
+    def test_ave_gpa(self):
+        driver = self.driver
+        #driver.refresh()
+        #TODO: Issue, without the sleep delay, throwing staleelement exception
+        time.sleep(3)
+        #Go to computer science page
+        termId = 'termChoice2'
+        subjectValue = '266'
+        self.findSubject(termId, subjectValue)
+        time.sleep(7)
+        #First courseResult card, block with ave gpa
+        aveGPA = driver.find_element_by_xpath("//tr[@class='courseResult']/td[7]")
+        self.assertIn("Ave. GPA: ", aveGPA.text)
+        self.assertIn("2.971", aveGPA.text)    
+
+
+    def test_grades_dropdown(self):
+        driver = self.driver
+        time.sleep(3)
+        #Go to computer science page
+        termId = 'termChoice2'
+        subjectValue = '266'
+        self.findSubject(termId, subjectValue)
+        dropdown = driver.find_element_by_xpath("//tr[@class='courseResult']/td[@colspan='8']/a[text()='grades']")
+        dropdown.click()
+        time.sleep(7)
+        #Add assertion for grades dropdown
+        gradeTable = driver.find_element_by_xpath("//table[@class='distTable']")
+        fall2016gpa = gradeTable.find_element_by_xpath("/tr[2]/td[3]")
+        assertEqual("3.104", fall2016gpa.text)         
 
     #tear down after each test
     def tearDown(self):
@@ -86,6 +120,26 @@ class GradeDistribution(unittest.TestCase):
         passbox.send_keys(password)
         time.sleep(1)
         button.click()
+
+    def findSubject(self, termId, subjectValue):
+        driver = self.driver
+        term = driver.find_element_by_xpath("//input[@id='" + termId + "']")
+        #try:
+        #element_present = EC.presence_of_element_located((By.XPATH, driver.find_element_by_xpath("//option[text()='ACTUARIAL SCIENCE']")))
+        #WebDriverWait(driver, 10).until(element_present)
+            #subject.click()        
+        #except Exception as e:
+        #    print(str(e))
+        #    print("Couldn't get subject")
+        subject = driver.find_element_by_xpath("//option[@value='" + subjectValue + "']")
+        findButton = driver.find_element_by_xpath("//button[@class='findBtn']")
+        
+        time.sleep(1)
+        term.click()
+        subject.click()
+        findButton.click()
+        time.sleep(2)
+        
 
 if __name__ == "__main__":
     unittest.main()
