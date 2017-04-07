ca70a1b096dcd6db20b3ddb46d716664544be8ff
GraysonFreking <grayson_freking@msn.com>
2017-04-06 19:11:02 -0500

    Map Spider / JSON updated

--- PythonScripts/mapSpider.py
+++ PythonScripts/mapSpider.py
@@ -16,11 +16,11 @@ class MapSpider(scrapy.Spider):
 		#Selects each row from the table and returns as a list
 		for row in response.css(ROW_SELECTOR):
 			#::text fetches the text inside a tag
-			BUILDING_SELECTOR = 'td ::text'
+			BUILDING_SELECTOR = 'tr th ::text'
 			#Selects the href attribute from td tag
 			LINK_SELECTOR = 'td a ::attr(href)'
 			yield {
 				#extract first: get the first element that matches selector
 				'building': row.css(BUILDING_SELECTOR).extract_first(),
-				'link': 'http://maps.wisc.edu/' + row.css(LINK_SELECTOR).extract_first(),
+				'link': 'http://maps.wisc.edu' + row.css(LINK_SELECTOR).extract_first(),
 			}
