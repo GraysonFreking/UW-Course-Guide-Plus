77789939965c03b9bc1c335e06df7a3adaa93816
babramczyk <bjabramczyk@gmail.com>
2017-04-24 09:11:39 -0500

    Browser action now opens Course Guide in new tab (issue #7)

--- Extension/js/browser_action.js
+++ Extension/js/browser_action.js
@@ -1,4 +1,8 @@
-// Brings user to course guide on icon click
+// Brings user to course guide in new tab on icon click
 chrome.browserAction.onClicked.addListener(function(tab) {
-    chrome.tabs.update(tab.id, { url: "https://my.wisc.edu/portal/p/CourseGuide-Browse-Courses/detached/action.uP?pCm=view&pP_action=advancedSearch" });
+    // Deprecated: Opens in current tab
+    // chrome.tabs.update(tab.id, { url: "https://my.wisc.edu/portal/p/CourseGuide-Browse-Courses/detached/action.uP?pCm=view&pP_action=advancedSearch" });
+
+    // Opens in new tab
+    chrome.tabs.create({ url: "https://my.wisc.edu/portal/p/CourseGuide-Browse-Courses/detached/action.uP?pCm=view&pP_action=advancedSearch"});
 });
